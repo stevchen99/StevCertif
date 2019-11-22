6f078aae7fe844019cbcb47cf199e75c0feb9203
stevchen99 <56722246+stevchen99@users.noreply.github.com>
2019-11-22 22:57:01 +0100

    4addd

--- src/test_data.js
+++ src/test_data.js
@@ -2,34 +2,46 @@ export default [
   {
     "HARD-SOFT": "HARDWARE",
     TECHNO: "FPGA",
-    TYPE: "Attestation",
+    TYPE: "Certificat",
     LANG: "Allemand",
-    QUAND: "2001",
+    QUAND: "2002",
     ORG: "Uni-KL",
-    LIBELLE: "FPGA--",
-    DESCRIPTION: "",
-    Cert: "image1.PNG"
+    LIBELLE: "Satelite Radio System",
+    DESCRIPTION: "Turbo Decoder avec FPGA",
+    Cert: "ZMK.PNG"
   },
   {
     "HARD-SOFT": "SOFTWARE",
-    TECHNO: "DBase",
-    TYPE: "Certificat",
-    LANG: "Indonesian",
-    QUAND: "2001",
-    ORG: "Purwadhika",
-    LIBELLE: "qsdfqs",
-    DESCRIPTION: "",
-    Cert: "defvest.PNG"
+    TECHNO: "PHP",
+    TYPE: "Attestation",
+    LANG: "Allemand",
+    QUAND: "2000",
+    ORG: "Cytexx",
+    LIBELLE: "Dev Web",
+    DESCRIPTION: "Création site Internet",
+    Cert: "Cytexx.PNG"
+  },
+  {
+    "HARD-SOFT": "SOFTWARE",
+    TECHNO: "PHP",
+    TYPE: "Attestation",
+    LANG: "Allemand",
+    QUAND: "2000",
+    ORG: "1Plus",
+    LIBELLE: "Dev Web",
+    DESCRIPTION: "Responsable Informatique TPE",
+    Cert: "OnePlus.PNG"
   },
   {
     "HARD-SOFT": "SOFTWARE",
     TECHNO: "Python",
-    TYPE: "Certificat",
+    TYPE: "Attestation",
     LANG: "Française",
     QUAND: "2019",
     ORG: "MOOC",
     LIBELLE: "Apprendre à coder avec Python",
-    DESCRIPTION: "Utilise Python 3 comme clé pour vous ouvrir la porte de cette connaissance informatique.",
+    DESCRIPTION:
+      "Utilise Python 3 comme clé pour vous ouvrir la porte de cette connaissance informatique.",
     Cert: "Python1.PNG"
   }
 ];
